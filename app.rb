5c8fc8f0751c3f89b1992f925c02e5ce02ba5fed
Neil Seejoor <nvseejoor@gmail.com>
2018-04-11 06:19:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Neil "
+  end
+  get '/hometown' do
+    "My hometown is Queens"
+  end
+  get '/favorite-song' do
+    "My favorite song is Kanye"
+  end
+
 end
